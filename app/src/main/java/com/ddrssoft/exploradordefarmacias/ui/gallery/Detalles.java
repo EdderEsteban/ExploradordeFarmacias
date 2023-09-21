886a5c80a1e031366a0760f6db8933b04edb53f9
EdderEsteban <edder709@gmail.com>
2023-09-20 23:50:18 -0300

    Commit Final

--- app/src/main/java/com/ddrssoft/exploradordefarmacias/ui/gallery/Detalles.java
+++ app/src/main/java/com/ddrssoft/exploradordefarmacias/ui/gallery/Detalles.java
@@ -1,35 +1,51 @@
 package com.ddrssoft.exploradordefarmacias.ui.gallery;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 import com.ddrssoft.exploradordefarmacias.R;
+import com.ddrssoft.exploradordefarmacias.modelo.Farmacia;
 
+import java.util.List;
 
 public class Detalles extends Fragment {
 
-
-    public Detalles() {
-        // Required empty public constructor
-    }
-
-
+    private ImageView ivFoto;
+    private TextView tvTelefono;
+    private TextView tvHorario;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detalles, container, false);
 
-    }
+        ivFoto = view.findViewById(R.id.ivfoto);
+        tvTelefono = view.findViewById(R.id.tvTelefono);
+        tvHorario = view.findViewById(R.id.tvHorario);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalles, container, false);
+        // Recuperar los datos de la farmacia del Bundle
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            List<Farmacia> farmacias = (List<Farmacia>) bundle.getSerializable("farmacia");
+            if (farmacias != null && farmacias.size() > 0) {
+                // Mostrar los datos de la primera farmacia (puedes ajustar esto según tus necesidades)
+                Farmacia farmacia = farmacias.get(0);
+                tvTelefono.setText(String.valueOf(farmacia.getTelefono()));
+                tvHorario.setText(farmacia.getHorario());
+
+                // Cargar la imagen usando Glide (asegúrate de tener la URL correcta en farmacia.getFoto())
+                Glide.with(requireContext())
+                        .load(farmacia.getFoto())
+                        .into(ivFoto);
+            }
+        }
+
+        return view;
     }
 }
--- app/src/main/java/com/ddrssoft/exploradordefarmacias/ui/gallery/FarmaciaAdapter.java
+++ app/src/main/java/com/ddrssoft/exploradordefarmacias/ui/gallery/FarmaciaAdapter.java
@@ -1,20 +1,14 @@
 package com.ddrssoft.exploradordefarmacias.ui.gallery;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.ddrssoft.exploradordefarmacias.R;
 import com.ddrssoft.exploradordefarmacias.modelo.Farmacia;
 
@@ -43,12 +37,7 @@ public class FarmaciaAdapter extends RecyclerView.Adapter<FarmaciaAdapter.ViewHo
     public void onBindViewHolder(@NonNull FarmaciaAdapter.ViewHolder holder, int position) {
         holder.nombre.setText(farmacia.get(position).getNombre());
         holder.direccion.setText(farmacia.get(position).getDireccion());
-        holder.telefono.setText(farmacia.get(position).getTelefono());
-        holder.horario.setText(farmacia.get(position).getHorario());
-        Glide.with(context)
-                .load(farmacia.get(position).getFoto())
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(holder.foto);
+
     }
 
     @Override
@@ -59,16 +48,12 @@ public class FarmaciaAdapter extends RecyclerView.Adapter<FarmaciaAdapter.ViewHo
     public class ViewHolder extends RecyclerView.ViewHolder{
         private TextView nombre;
         private TextView direccion;
-        private TextView telefono;
-        private TextView horario;
-        private ImageView foto;
+
         public ViewHolder(@NonNull View view) {
             super(view);
             nombre = itemView.findViewById(R.id.tvNombre);
             direccion = itemView.findViewById(R.id.tvDireccion);
-            telefono = itemView.findViewById(R.id.tvTelefono);
-            horario = itemView.findViewById(R.id.tvHorario);
-            foto = itemView.findViewById(R.id.ivfoto);
+
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
